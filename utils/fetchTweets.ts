6a11aabee80a31e50c567c7c823258ddb757ce2a
william <william@williams-MacBook-Pro.local>
2022-06-03 15:02:27 +0100

    1

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,7 +1,8 @@
 import { Tweet } from "../typings";
 
 export const fetchTweets = async ()=>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweet`)
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweet`)
+    const res = await fetch(`/api/getTweet`)
 
     const data = await res.json();
     const tweets:Tweet[] = data.tweets;
