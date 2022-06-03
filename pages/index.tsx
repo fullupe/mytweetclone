bc1d207cd85d847c04727b2f6e7d0dab464ede93
william <william@williams-MacBook-Pro.local>
2022-06-03 14:38:28 +0100

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
 import React from 'react'
 import Feed from '../components/Feed'
 import SideBar from '../components/SideBar'
